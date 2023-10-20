61666046c6cbea71d57af5200daf457c2fc1a826
piedpiper36 <vaskmov@gmail.com>
2023-10-20 11:38:01 +0530

    web_scraping: Added Support for dry_run and recovery modes: 	dry-run mode : Fetches all the links and saves them to text file. 	Skips saving them as PDF. 	recovery mode: Whenever a crash occcurs in previous run. 	Recovery mode tries to continue it from point of crash if the persistent files are 	available

--- src/bs.py
+++ src/bs.py
@@ -6,64 +6,86 @@ from bs4 import BeautifulSoup
 from common.utils import url_to_str
 
 
-def scrape_root_url(start_url, dir_name):
+def update_toVisit_n_touched_lists(to_visit, touched_urls, new_link):
+    to_visit.append(new_link)
+    touched_urls.add(new_link)
+
+
+def scrape_root_url(start_url, dir_name, recovery_mode=False, dry_run=False):
     to_visit = []
     touched_urls = set()
-    if os.path.exists('touched_urls.txt') and os.path.exists('to_visit.txt'):
-        # Read touched_urls
-        with open('touched_urls.txt', 'r') as f:
-            lines = []
-            for line in f:
-                touched_urls.add(line.strip())
-        # Read tovisit links
-        with open('to_visit.txt', 'r') as f:
-            lines = []
-            for line in f:
-                to_visit.append(line.strip())
+
+    if recovery_mode:
+        if os.path.exists("touched_urls.txt") and os.path.exists("to_visit.txt"):
+            # Read touched_urls
+            with open("touched_urls.txt", "r") as f:
+                lines = []
+                for line in f:
+                    touched_urls.add(line.strip())
+            # Read tovisit links
+            with open("to_visit.txt", "r") as f:
+                lines = []
+                for line in f:
+                    to_visit.append(line.strip())
+        else:
+            to_visit.append(start_url)
+            touched_urls.add(start_url)
     else:
         to_visit.append(start_url)
         touched_urls.add(start_url)
 
     try:
+        visited_url_count = 0
         while to_visit:
             linkToVisit = to_visit.pop(0)
-            print(linkToVisit, '==== More to go : ' + str(len(to_visit)))
+            print(
+                f"url:{linkToVisit},\tTotal urls visited:{visited_url_count}"
+                f"\tTouched urls:{len(touched_urls)}"
+                f"\tLinks to visit: {len(to_visit)}"
+            )
             response = requests.get(linkToVisit)
             source = BeautifulSoup(response.text, "html.parser")
             divsFound = source.find_all("div", {"class": "d-lg-flex"})
+
             if len(divsFound) < 2:
                 continue
+
             div = divsFound[1]
-            linksFound = div.find_all('a')
+            linksFound = div.find_all("a")
+
             for link in linksFound:
                 if link.has_attr("href"):
                     href = link["href"]
-                    if href[0] == "#" and len(href) > 1 and '#' not in linkToVisit:
+                    if href[0] == "#" and len(href) > 1 and "#" not in linkToVisit:
                         new_link = linkToVisit + href
                         if new_link not in touched_urls:
-                            to_visit.append(new_link)
-                            touched_urls.add(new_link)
+                            update_toVisit_n_touched_lists(
+                                to_visit, touched_urls, new_link
+                            )
                     if href[0] == "?" and len(href) > 1:
                         new_link = linkToVisit + href
                         if new_link not in touched_urls:
-                            to_visit.append(new_link)
-                            touched_urls.add(new_link)
+                            update_toVisit_n_touched_lists(
+                                to_visit, touched_urls, new_link
+                            )
                     elif href[0] == "/":
                         new_link = start_url + href
                         if new_link not in touched_urls:
-                            to_visit.append(new_link)
-                            touched_urls.add(new_link)
+                            update_toVisit_n_touched_lists(
+                                to_visit, touched_urls, new_link
+                            )
 
-            end_point = linkToVisit.removeprefix(start_url)
-            with open('touched_urls.txt', 'a') as the_file:
-                the_file.write(linkToVisit + '\n')
-
-            pdf_name = os.path.join(dir_name, url_to_str(end_point) + ".pdf")
-            pdfkit.from_url(linkToVisit, pdf_name)
+            if not dry_run:
+                end_point = linkToVisit.removeprefix(start_url)
+                pdf_name = os.path.join(dir_name, url_to_str(end_point) + ".pdf")
+                pdfkit.from_url(linkToVisit, pdf_name)
+            visited_url_count += 1
     except Exception:
-        print('Exception Occured')
+        print("Exception Occured")
     finally:
-        # Touched urls are already saved.
-        # Now try to save to_visit urls.
-        with open('to_visit.txt','w') as file:
-	        file.write('\n'.join(to_visit))
+        if dry_run:
+            # Save visited & touched urls.
+            with open("to_visit.txt", "w") as file:
+                file.write("\n".join(to_visit))
+            with open("touched_urls.txt", "a") as the_file:
+                the_file.write("\n".join(touched_urls))
--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 from common.utils import url_to_str
 from common.utils import create_directory
@@ -6,20 +6,49 @@ from bs import scrape_root_url
 
 
 def main():
-    # Access command-line arguments using sys.argv
-    # sys.argv[0] is the script name, and subsequent elements are arguments
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <base_url>")
-        return
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description="application to scrape the given Web")
 
-    start_url = sys.argv[1]
+    # Define the mandatory URL argument
+    parser.add_argument("url", type=str, help="A URL string")
+
+    # Define the optional boolean argument to start the app from reading urls from files
+    parser.add_argument(
+        "--recovery-mode",
+        action="store_false",
+        help="Restart the app using the urls dumped in the previous run",
+    )
+
+    # Define the optional boolean argument to run the app without dumping urls as pdfs.
+    # This will help us to prepare the list of all the urls needs to be dumped.
+    parser.add_argument(
+        "--dry-run",
+        action="store_false",
+        help="Rrun the app without dumping urls as pdfs",
+    )
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Access the URL and boolean values
+    start_url = args.url
+    recovery_mode = args.recovery_mode
+    dry_run = args.dry_run
+
+    # Use the input arguments in your script
+    print(f"URL: {start_url}")
+    if recovery_mode:
+        print(
+            f"Restarting the app by using the urls saved in "
+            f"'tovisit.txt' & 'touched_urls.txt' as input"
+        )
 
     print(f"Start url: {start_url}")
 
     dir_name = url_to_str(start_url)
     create_directory(dir_name)
 
-    scrape_root_url(start_url, dir_name)
+    scrape_root_url(start_url, dir_name, recovery_mode=recovery_mode, dry_run=dry_run)
 
 
 if __name__ == "__main__":
